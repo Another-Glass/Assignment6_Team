acbe9c3558e74b330b59d4adc2e6dfd2a56af39e
raejun92 <67402180+raejun92@users.noreply.github.com>
2021-11-19 15:42:11 +0900

    [Feat]: history API 구성 (#2)
    1. history 컨트롤러에서 historyId를 받아와 숫자인 형식이 아닌 경우 예외
       처리
    2. history 컨트롤러에서 데이터베이스에 존재하지 않는 historyId 예외 처리
    3. history 컨트롤러에서 계산한 최종비용이 음수일 경우 오류 처리

--- src/controllers/historyController.js
+++ src/controllers/historyController.js
@@ -1,12 +1,30 @@
 const { statusCode, responseMessage } = require('../globals');
 const { resFormatter } = require('../utils');
 const logger = require('../utils/logger');
-const { ValidationError } = require('../utils/errors/commonError');
-const historyService = require('../services/historyService')
+const { ValidationError, EntityNotExistError, InternalServerError } = require('../utils/errors/commonError');
+const historyService = require('../services/historyService');
+// const costCalculator = require('../libs/costChains');
 //이용요금 계산
 exports.getFinalCost = async (req, res, next) => {
 	try {
 		const historyId = req.params.historyId;
+
+		if (!historyId || isNaN(historyId) || historyId.includes(' '))
+			throw new ValidationError();
+		// const isHistory = await getLatestHistoryOfUser(historyId);
+
+		// if (!isHistory)
+		// 	throw new EntityNotExistError();
+		
+		// const finalCost = await costCalculator.calculateCost(historyId);
+		const finalCost = 1000;
+
+		if (finalCost < 0)
+			throw new InternalServerError();
+
+		return res
+			.status(statusCode.OK)
+			.send(resFormatter.success(responseMessage.FINALCOST_SUCCESS, finalCost));
 	} catch (err) {
 		next(err);
 	}
