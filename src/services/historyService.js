e1c5180f4dbb14f7a90acbb0ac5329b086db00a6
Makkiato <shinecraft@naver.com>
2021-11-19 20:28:02 +0900

    [HOTFIX]: isTransport가 무조건 최근 사용내역에 대해서만 정산과정을 거치는 버그 수정
    1. src/services/historyService.js (line 30)의 쿼리수정

--- src/services/historyService.js
+++ src/services/historyService.js
@@ -24,8 +24,9 @@ exports.readHistory = async historyId => {
 /**
  * 30분 이내 환승이면 true, 아니면 false 반환
  * @param {사용자 아이디} userId 
+ * @return {value : 30분 이내 재사용 여부}
  */ 
-exports.isTransport = async (userId) => {
+exports.isTransport = async (userId,historyId) => {
   const query = `
   SELECT 
 	  HOUR(time_gap.diff) = 0 AND MINUTE(time_gap.diff) < 31
@@ -38,12 +39,12 @@ exports.isTransport = async (userId) => {
         (
           SELECT startTime 
           FROM histories 
-          WHERE id = (SELECT id FROM (SELECT id, startTime, endTime FROM histories WHERE userId = :userId) user_only ORDER BY startTime DESC LIMIT 1)
+          WHERE id = (SELECT id FROM (SELECT id, startTime, endTime FROM histories WHERE userId = :userId) user_only WHERE id = :historyId)
         ), 
         (
           SELECT endTime 
           FROM histories 
-          WHERE id = (SELECT id FROM (SELECT id, startTime, endTime FROM histories WHERE userId = :userId) user_only ORDER BY startTime DESC LIMIT 1 OFFSET 1)
+          WHERE id = (SELECT id FROM (SELECT id, startTime, endTime FROM histories WHERE userId = :userId) user_only WHERE id < :historyId ORDER BY startTime DESC LIMIT 1)
         )
       ) AS diff	 
   ) time_gap    
@@ -51,7 +52,8 @@ exports.isTransport = async (userId) => {
   try {
     let result = await models.sequelize.query(query,{
       replacements : {
-        userId: userId
+        userId: userId,
+        historyId : historyId
       },
       type: models.sequelize.QueryTypes.SELECT
     })
@@ -72,6 +74,7 @@ exports.isTransport = async (userId) => {
 /**
  * 1분 이내 사용이면 true, 아니면 false 반환
  * @param {사용내역 아이디} historyId 
+ * @return {value : 1분내 반납 여부}
  */
 exports.returnInMinute = async (historyId) => {
   const query = `
@@ -102,15 +105,112 @@ exports.returnInMinute = async (historyId) => {
 
 //변동요금
 
+/**
+ * 금지구역 반납 확인
+ * @param {검사할 좌표의 geoJSON String} geoJSON
+ * @return {value : 금지구역 포함 유무}
+ */
 
-exports.isInForbidden = async ()=>{
+exports.isInForbidden = async (geoJSON)=>{
+  const query = `
+  SELECT
+    EXISTS (SELECT * FROM forbiddenAreas WHERE ST_INTERSECTS(boundary,ST_GeomFromGeoJSON(:geoJSON,1,0)))
+  AS isForbidden    
+  `
 
+  try {
+    let result = await models.sequelize.query(query,{
+      replacements : {
+        geoJSON: geoJSON
+      },
+      type: models.sequelize.QueryTypes.SELECT
+    })
+    let toReturn = {}
+    if(result[0].isForbidden){
+      toReturn.value = true
+      return toReturn;
+    } else {
+      toReturn.value = false
+      return toReturn;
+    }
+  } catch (err) {
+    throw err
+  }
+}
+/** 
+ * 활동구역 내 반납 확인
+ * @param {검사할 활동 지역의 ID}areaId
+ * @param {검사할 좌표의 geoJSON String} geoJSON
+ * @return {value : 허용구역 내 포함 여부, distance : 허용구역 벗어났을시 거리}
+ */
+exports.isInAllowedArea = async (areaId,geoJSON)=>{
+  const query = `
+  SELECT	
+    IF (ST_INTERSECTS(BOUNDARY,ST_GeomFromGeoJSON(:geoJSON,1,0)),
+    0,
+    ST_DISTANCE_SPHERE(center,ST_GeomFromGeoJSON(:geoJSON,1,0))	
+    )
+  AS distance	
+  FROM 
+	  areas 
+  WHERE id = :areaId     
+  `
+  try {
+    let result = await models.sequelize.query(query,{
+      replacements : {
+        areaId : areaId,
+        geoJSON: geoJSON
+      },
+      type: models.sequelize.QueryTypes.SELECT
+    })
+    let toReturn = {}
+    
+    if(!result[0].distance){
+      toReturn.value = true
+      return toReturn;
+    } else {
+      toReturn.value = false
+      toReturn.distance = result[0].distance
+      return toReturn;
+    }
+  } catch (err) {
+    throw err
+  }  
 }
 
-exports.isInAllowedArea = async ()=>{
-  
+/**
+ * 주차구역내 반납 확인
+ * @param {검사할 좌표의 geoJSON String} geoJSON
+ * @return {value : 주차구역 내 포함 여부}
+ */
+exports.isInParkingZone = async (geoJSON)=>{
+  const query = `
+  SELECT
+    EXISTS
+    (
+      SELECT id 
+      FROM parkingzones 
+      WHERE ST_DISTANCE_SPHERE(centerPoint,ST_GeomFromGeoJSON(:geoJSON,1,0)) < radius
+    )
+  AS inParkingZone
+  `
+  try {
+    let result = await models.sequelize.query(query,{
+      replacements : {
+        geoJSON: geoJSON
+      },
+      type: models.sequelize.QueryTypes.SELECT
+    })
+    let toReturn = {}
+    if(result[0].inParkingZone){
+      toReturn.value = true
+      return toReturn;
+    } else {
+      toReturn.value = false
+      return toReturn;
+    }
+  } catch (err) {
+    throw err
+  }
 }
 
-exports.isInParkingZone = async ()=>{
-  
-}
