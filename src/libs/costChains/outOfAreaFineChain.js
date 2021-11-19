155b976418bbff22afa18bfc815459a19c572f85
epitone <epitoneplus@gmail.com>
2021-11-19 16:46:57 +0900

    [HOTFIX]: chain에 try, catch문 추가
    1. outOfAreaFineChain try, catch 문 추가
    2. discountChain try, catch 문 추가
    3. forbiddenAreaFineChain try, catch 문 추가

--- src/libs/costChains/outOfAreaFineChain.js
+++ src/libs/costChains/outOfAreaFineChain.js
@@ -7,15 +7,22 @@ class outOfAreaFineChain extends  CostChainBase{
 	}
 	
   async isInAllowedArea(areaId, point) {
-      return await historyService.isInAllowedArea(areaId, point); 
+		try {
+			return await historyService.isInAllowedArea(areaId, point); 
+		} catch (err) {
+			throw err;
+		}   
   }
 
 	async calculateCost(data){
-		if(!this.isInAllowedArea(data.areaId, data.endPoint).value) {
-      data.finalCost = data.finalCost + this.isInAllowedArea(data.areaId, data.endPoint).distance * 100;        
-    }
-		
-		return await this.goToNextChain(data);
+		try {
+			if(!this.isInAllowedArea(data.areaId, data.endPoint).value) {
+				data.finalCost = data.finalCost + this.isInAllowedArea(data.areaId, data.endPoint).distance * 100;        
+			}
+			return await this.goToNextChain(data);
+		} catch(err) {
+			throw err;
+		}
 	}
 }
 
