a7e5ab50fc4ee2fb9b39410156463bd4af0527c0
SibaDoge1 <rladus753159@gmail.com>
2021-11-19 17:11:02 +0900

    [fix]: api 응답값 수정 및 코드스타일 정리
    - api 응답값 설계에 맞게 수정
    - 체인 관리쪽 코드 스타일 정리

--- src/libs/costChains/costCalculator.js
+++ src/libs/costChains/costCalculator.js
@@ -2,7 +2,7 @@ const { getOwningArea } = require("../../services/deerService");
 const { readHistory } = require("../../services/historyService");
 const logger = require("../../utils/logger");
 
-//체인을 관리하여 최종 사용하는 클래스
+//체인 전체를 관리하는 매니저 클래스
 class CostCalculator {
 	constructor(){
 		this._frontChain;
@@ -10,6 +10,7 @@ class CostCalculator {
 		this.data;
 	}
 
+	//체인 추가
 	addChain(chain){
 		if(this._frontChain === undefined)
 			this._frontChain = chain;
@@ -20,29 +21,33 @@ class CostCalculator {
 		this._rearChain = chain;
 	}
 
+	//체인 실행
 	async calculateCost(historyId){
-		await this.preprocessData(historyId);
+		this.data = await this.preprocessData(historyId);
+
 		return await this._frontChain.calculateCost(this.data);
 	}
 
+	//체인 실행 전 필요한 데이터를 전처리
 	async preprocessData(historyId){
-		const history = await readHistory(historyId);
-		const deerAreaId = await getOwningArea(history.deerId);
+			const history = await readHistory(historyId);
+			const deerAreaId = await getOwningArea(history.deerId);
 
-		this.data = {	
-			historyId: history.id,
-			userId: history.userId,
-			useDeerName: history.deerId,
-			deerAreaId: deerAreaId, //킥보드 소유한 지역id
-			baseCost: 0, //- 최초엔 0, defaultCostChains을 거치면 기본값 들어옴 (할인안된 최초 기본+시간당요금)
-			finalCost: 0,
-			startPoint: history.startPoint,  ///mysql의 ST_AsGeoJSON함수를 살펴볼것. 그 값 그대로 넣을예정
-			endPoint: history.endPoint,
-			startTime: history.startTime,
-			endTime: history.endTime,
-		}
-		logger.logWithTag('chain preprocessed data is : '+ JSON.stringify(this.data), 'src:costChain')
-		
+			let data = {	
+				historyId: history.id,
+				userId: history.userId,
+				useDeerName: history.deerId,
+				deerAreaId: deerAreaId, 				//킥보드 소유한 지역id
+				baseCost: 0, 										//- 최초엔 0, defaultCostChains을 거치면 기본값 들어옴 (할인안된 최초 기본+시간당요금)
+				finalCost: 0, 									//- 최초엔 0, defaultCostChains을 거치면 기본값 들어옴 (할인안된 최초 기본+시간당요금)
+				startPoint: history.startPoint, ///mysql의 ST_AsGeoJSON함수를 살펴볼것. 그 값 그대로 넣을예정
+				endPoint: history.endPoint,			///mysql의 ST_AsGeoJSON함수를 살펴볼것. 그 값 그대로 넣을예정
+				startTime: history.startTime,
+				endTime: history.endTime,
+			}
+
+			logger.logWithTag('Chain preprocessed data IS : '+ JSON.stringify(this.data), 'src:costChain')
+			return data;
 	}
 }
 
