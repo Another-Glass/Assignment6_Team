8a108e26aee3f95e79513596b49b356945ef72ec
Donghoon Lee <cjswodlehdgn@gmail.com>
2021-11-19 16:15:06 +0900

    [feat]: 1분 이내 무료 체인 구현 및 리턴값 수정 (#16)
    1. 무료 체인 구현
    2. defaultCostChain의 return문 수정

--- src/libs/costChains/defaultCostChain.js
+++ src/libs/costChains/defaultCostChain.js
@@ -1,15 +1,11 @@
-/* db에서 받아서 data에 
-* baseCost:, - (할인안된 최초 기본+시간당요금)
-* 이 프로퍼티에도 값 넣어줘야함
-*/
 const defaultCostService = require('../../services/defaultCostService');
 const CostChainBase = require('./costChainBase');
 
-class defaultCostChain extends CostChainBase{
-	constructor(nextChain){
-		super(nextChain)
-	}
-	
+class defaultCostChain extends CostChainBase {
+  constructor(nextChain) {
+    super(nextChain);
+  }
+
   calculateMinute(startTime, endTime) {
     const endTimeMs = new Date(endTime).getTime();
     const startTimeMs = new Date(startTime).getTime();
@@ -19,19 +15,20 @@ class defaultCostChain extends CostChainBase{
     return minutes;
   }
 
-	async calculateCost(data) {
-		
-		const { id, startTime, endTime } = await defaultCostService.getLatestHistoryOfUser(data.historyId);
-    const { id, priceBase, pricePerMinute } = await defaultCostService.getAreaPrice(data.deerAreaId);
-		
+  async calculateCost(data) {
+    const { id, startTime, endTime } =
+      await defaultCostService.getLatestHistoryOfUser(data.historyId);
+    const { id, priceBase, pricePerMinute } =
+      await defaultCostService.getAreaPrice(data.deerAreaId);
+
     const rideMinutes = this.calculateMinute(startTime, endTime);
     const defaultCost = priceBase + pricePerMinute * rideMinutes;
 
     data.baseCost = defaultCost;
     data.finalCost = defaultCost;
 
-		return goToNextChain(data);
-	}
+    return await this.goToNextChain(data);
+  }
 }
 
 module.exports.defaultCostChain = defaultCostChain;
