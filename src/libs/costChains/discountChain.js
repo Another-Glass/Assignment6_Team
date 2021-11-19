3792de6874d10b7c92644e20f813dd0a026ced51
epitone <epitoneplus@gmail.com>
2021-11-19 15:09:52 +0900

    [feat]: 할인 계산 체인 로직 추가
    1. 파킹존 여부에 따른 할인 정책 코드 추가
    2. 오타 수정

---
+++ src/libs/costChains/discountChain.js
@@ -0,0 +1,23 @@
+const CostChainBase = require('./costChainBase')
+const historyService = require('../../services/historyService');
+
+class DiscountChain extends CostChainBase{
+	constructor(nextChain){
+		super(nextChain)
+	}
+	
+  async isInParkingZone(endPoint) {
+    const isParkingZone = await historyService.isInParkingZone(endPoint);
+    if(isParkingZone) return true
+    else return false;
+  }
+
+	calculateCost(data) {    
+    if(this.isInParkingZone(data.endPoint)) {
+      data.finalCost = data.finalCost * 0.7
+    } 
+		return this.goToNextChain(data);
+	}
+}
+
+module.exports.DiscountChain = DiscountChain;
