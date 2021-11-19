1e5047030d393a4a00463672762349479464df35
SibaDoge1 <rladus753159@gmail.com>
2021-11-19 18:38:20 +0900

    [fix]:목록조회 api들 라우터 수정
    - 목록조회api들 globalRouter로 옮기고 주소에 /listing 제거

--- src/globals/routes.js
+++ src/globals/routes.js
@@ -3,17 +3,12 @@
 // Root
 const ROOT = '/';
 
-// User
-const USER_SIGNUP = '/user';
-const USER_SIGNIN = '/token';
-
 // history
 const HISTORY = '/histories';
 const HISTORY_DETAIL = '/:historyId';
 const COST = '/cost';
 
 // listing
-const LISTING = '/listing';
 const USER = '/users';
 const DEER = '/deers';
 const AREA = '/areas';
@@ -25,7 +20,6 @@ const routes = {
   history: HISTORY,
   historyDetail: HISTORY_DETAIL,
   cost: COST,
-  listing: LISTING,
   user: USER,
   deer: DEER,
   area: AREA,
