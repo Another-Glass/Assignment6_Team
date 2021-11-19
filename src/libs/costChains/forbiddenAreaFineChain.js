3d321d4016390810834eb9d83d0e409bd06e279a
epitone <epitoneplus@gmail.com>
2021-11-19 20:41:10 +0900

    [fix]: 체인 상수값 env에 추가
    1. 할인 계산, 반납금지, 지역범위 계산 로직 중 상수 값 env에 추가

--- src/libs/costChains/forbiddenAreaFineChain.js
+++ src/libs/costChains/forbiddenAreaFineChain.js
@@ -1,11 +1,13 @@
 const CostChainBase = require('./costChainBase')
 const historyService = require('../../services/historyService');
+const dotenv = require('dotenv');
+dotenv.config();
 
 class ForbiddenAreaFineChain extends CostChainBase{
 	constructor(nextChain){
 		super(nextChain)
 	}
-	
+
   async isInForbidden(endPoint) {
 		try {
 			const isForbidden = await historyService.isInForbidden(JSON.stringify(endPoint).replace('"','\"'));
@@ -18,7 +20,7 @@ class ForbiddenAreaFineChain extends CostChainBase{
 	async calculateCost(data) { 
 		try {
 			if(await this.isInForbidden(data.endPoint)) {
-				data.finalCost = data.finalCost + 6000;
+				data.finalCost = data.finalCost + process.env.FORBIDDEN_AREA_FINE;
 			}
 			return await this.goToNextChain(data);
 		} catch(err) {
